8d73c9eea2d01b51a4a618f790ee18719b8adf82
kamilrogala <contact@kamilrogala.it>
2018-04-07 22:56:54 +0200

    we're drawing single triangle

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,20 @@
-window.addEventListener('DOMContentLoaded',()=>{
-	//babel works perfect with bash commands	
+window.addEventListener('DOMContentLoaded', () => {
+	const canvasEl = document.querySelector('canvas');
+	const ctx = canvasEl.getContext('2d');
+	const cWidth = canvasEl.width;
+	const cHeight = canvasEl.height;
+	ctx.fillStyle = "#000";
+	ctx.fillRect(0, 0, cWidth, cHeight);
+	ctx.fillStyle = "rgba(205,205,0,.5)";
+	ctx.beginPath();
+	ctx.moveTo(random(cWidth), random(cHeight));
+	ctx.lineTo(random(cWidth), random(cHeight));
+	ctx.lineTo(random(cWidth), random(cHeight));
+	ctx.closePath();
+	ctx.fill();
+
 });
+
+function random(a) {
+	return Math.floor(Math.random() * a) + 1;
+}
