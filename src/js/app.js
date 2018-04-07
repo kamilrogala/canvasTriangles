240a786bd77551a7512918afda2baef5747501e5
kamilrogala <contact@kamilrogala.it>
2018-04-07 23:22:21 +0200

    now triangles are objects

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,49 @@
 window.addEventListener('DOMContentLoaded', () => {
-	const trianglesAmount = 10;
-	const canvasEl = document.querySelector('canvas');
-	const ctx = canvasEl.getContext('2d');
-	const cWidth = canvasEl.width;
-	const cHeight = canvasEl.height;
-	ctx.fillStyle = "#000";
-	ctx.fillRect(0, 0, cWidth, cHeight);
-	ctx.fillStyle = "rgba(205,205,0,.5)";
-	for (let i = 0; i < trianglesAmount; i++) {
-		drawTriangle();
+	class Triangle {
+		constructor(x1, y1, x2, y2, x3, y3) {
+			this.x1 = x1;
+			this.x2 = x2;
+			this.x3 = x3;
+			this.y1 = y1;
+			this.y2 = y2;
+			this.y3 = y3;
+		}
+
+		drawTriangle() {
+			ctx.beginPath();
+			ctx.moveTo(this.x1, this.y1);
+			ctx.lineTo(this.x2, this.y2);
+			ctx.lineTo(this.x3, this.y3);
+			ctx.closePath();
+			ctx.fill();
+		}
 	}
 
 	function random(a) {
 		return Math.floor(Math.random() * a) + 1;
 	}
+	const trianglesAmount = 10;
+	const canvasEl = document.querySelector('canvas');
+	const ctx = canvasEl.getContext('2d');
+	const cWidth = canvasEl.width;
+	const cHeight = canvasEl.height;
+	const trianglesArray = []
+	ctx.fillStyle = "#000";
+	ctx.fillRect(0, 0, cWidth, cHeight);
+	ctx.fillStyle = "rgba(205,205,0,.5)";
 
-	function drawTriangle() {
-		ctx.beginPath();
-		ctx.moveTo(random(cWidth), random(cHeight));
-		ctx.lineTo(random(cWidth), random(cHeight));
-		ctx.lineTo(random(cWidth), random(cHeight));
-		ctx.closePath();
-		ctx.fill();
+	for (let i = 0; i < trianglesAmount; i++) {
+		let newTriangle = new Triangle(
+			random(cWidth),
+			random(cHeight),
+			random(cWidth),
+			random(cHeight),
+			random(cWidth),
+			random(cHeight)
+		);
+		trianglesArray.push(newTriangle);
 	}
+	trianglesArray.forEach((singleTriangle) => {
+		singleTriangle.drawTriangle();
+	});
 });
