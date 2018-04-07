3bb906b33b44e890b5f8cc3bb34d487c3208e4da
kamilrogala <contact@kamilrogala.it>
2018-04-07 23:31:23 +0200

    triangles have different colours and opacity

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
 	class Triangle {
-		constructor(x1, y1, x2, y2, x3, y3) {
+		constructor(x1, y1, x2, y2, x3, y3, color) {
 			this.x1 = x1;
 			this.x2 = x2;
 			this.x3 = x3;
 			this.y1 = y1;
 			this.y2 = y2;
 			this.y3 = y3;
+			this.color = color;
 		}
 
 		drawTriangle() {
+			ctx.fillStyle = this.color;
 			ctx.beginPath();
 			ctx.moveTo(this.x1, this.y1);
 			ctx.lineTo(this.x2, this.y2);
@@ -30,16 +32,21 @@ window.addEventListener('DOMContentLoaded', () => {
 	const trianglesArray = []
 	ctx.fillStyle = "#000";
 	ctx.fillRect(0, 0, cWidth, cHeight);
-	ctx.fillStyle = "rgba(205,205,0,.5)";
 
 	for (let i = 0; i < trianglesAmount; i++) {
-		let newTriangle = new Triangle(
+		const R = Math.floor(Math.random() * 255) + 1;
+		const G = Math.floor(Math.random() * 255) + 1;
+		const B = Math.floor(Math.random() * 255) + 1;
+		const A = (Math.random()).toPrecision(2);
+		const color = `rgba(${R}, ${G}, ${B}, ${A})`;
+		const newTriangle = new Triangle(
 			random(cWidth),
 			random(cHeight),
 			random(cWidth),
 			random(cHeight),
 			random(cWidth),
-			random(cHeight)
+			random(cHeight),
+			color
 		);
 		trianglesArray.push(newTriangle);
 	}
